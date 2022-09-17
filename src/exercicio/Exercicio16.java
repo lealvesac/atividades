977c3d0a65d0e633241a7875548c32de3ed098bf
Leandro Alves <lealvesac@gmail.com>
2022-09-17 15:08:08 -0300

    Inicio resolução exercicio16

--- src/exercicio/Exercicio16.java
+++ src/exercicio/Exercicio16.java
@@ -6,5 +6,20 @@ usuário a quantidades de latas de tinta a serem compradas e o preço
 total*/
 package exercicio;
 
+import java.util.Scanner;
+
 public class Exercicio16 {
+        public static void main(String[] ags){
+
+            Scanner scan = new Scanner(System.in);
+
+            double valorLataTinta = 80.00;
+            double qtdLataTinta = 18.00;
+            double metrosQuadradosLataTinta = qtdLataTinta / 3;
+
+            System.out.print("Informe a area em metros quadrados a ser pintada: ");
+            double areaPintura = scan.nextDouble();
+
+        }
+
 }
