62a4795f7afd3af8da2584bfe7f186fe890dbb0a
Leandro Alves <lealvesac@gmail.com>
2022-09-18 15:47:04 -0300

    correção exercicio04

--- src/exercicio02/Exercicio04.java
+++ src/exercicio02/Exercicio04.java
@@ -2,5 +2,35 @@
 //consoante.
 package exercicio02;
 
+import java.util.Scanner;
+
 public class Exercicio04 {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Entre com uma letra: ");
+        String letra = input.next();
+
+        if (letra.length() > 1){
+            System.out.println("Não é uma letra válida!");
+        } else {
+            switch (letra){
+                case "a":
+                case "e":
+                case "i":
+                case "o":
+                case "u":
+                case "A":
+                case "E":
+                case "I":
+                case "O":
+                case "U":
+                    System.out.println(letra + " é uma VOGAL.");
+                break;
+                default:
+                    System.out.println(letra + " é uma CONSOANTE.");
+            }
+        }
+    }
 }
