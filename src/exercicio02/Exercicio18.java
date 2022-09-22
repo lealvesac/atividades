18092aad081bbbecac63488024e4a9804d79c061
Leandro Alves <lealvesac@gmail.com>
2022-09-22 19:46:36 -0300

    Adição de desafios para resolver do 18 ao 28.

---
+++ src/exercicio02/Exercicio18.java
@@ -0,0 +1,6 @@
+/*Faça um Programa que peça uma data no formato dd/mm/aaaa e
+determine se a mesma é uma data válida.*/
+package exercicio02;
+
+public class Exercicio18 {
+}
---
+++ src/exercicio02/Exercicio22.java
@@ -0,0 +1,6 @@
+/*Faça um Programa que peça um número inteiro e determine se ele é
+        par ou impar. Dica: utilize o operador módulo (resto da divisão)*/
+package exercicio02;
+
+public class Exercicio22 {
+}
