b47feff2e2249cdcc4b6982633a9a6a583bc2bbb
Leandro Alves <lealvesac@gmail.com>
2022-09-23 08:31:46 -0300

    resolução Exercicio20

--- src/exercicio02/Exercicio20.java
+++ src/exercicio02/Exercicio20.java
@@ -8,5 +8,28 @@
         10.*/
 package exercicio02;
 
+import java.util.Scanner;
+
 public class Exercicio20 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("CALCULADORA DE MÉDIA DE NOTAS!");
+        System.out.print("Informe a NOTA1: ");
+        double nota1 = input.nextDouble();
+        System.out.print("Informe a NOTA2: ");
+        double nota2 = input.nextDouble();
+        System.out.print("Informe a NOTA3: ");
+        double nota3 = input.nextDouble();
+
+        double media = (nota1 + nota2 + nota3) / 3;
+
+        if (media >= 7 && media < 10) {
+            System.out.println("Vocé esta APROVADO com uma média de " + media);
+        } else if (media < 7) {
+            System.out.println("Você esta REPROVADO com uma média de " + media);
+        } else {
+            System.out.println("PARABENS APROVADO COM DISTINÇÃO!!!");
+        }
+    }
 }
