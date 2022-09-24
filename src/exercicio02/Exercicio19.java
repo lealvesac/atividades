b56e0799c65326a4a88b639ea24ade49bb1b5c3f
Leandro Alves <lealvesac@gmail.com>
2022-09-24 10:36:13 -0300

    resolução Exercicio19

--- src/exercicio02/Exercicio19.java
+++ src/exercicio02/Exercicio19.java
@@ -7,5 +7,29 @@
         310,305, 301, 101, 311, 111, 25, 20, 10, 21, 11, 1, 7 e 16*/
 package exercicio02;
 
+import java.util.Scanner;
+
 public class Exercicio19 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Informe um numero inteiro menor que 1000.\n" +
+                "Vamos decompor ele para saber a quantidade de CENTENAS, DEZENAS e UNIDADE:");
+        int numInformado = input.nextInt();
+
+        int centena;
+        int dezena;
+        int unidade;
+
+        if (numInformado < 1000){
+            centena = numInformado / 100;
+            dezena = (numInformado / 10) % 10;
+            unidade = numInformado % 10;
+            System.out.println(centena + " , " +  dezena + ", " +  unidade);
+        } else {
+            System.out.println("Valos Informado INVALIDO!!!");
+        }
+
+
+    }
 }
