bb14f442c29a4dff5d44182ff1ad2acc6029d7eb
Leandro Alves <lealvesac@gmail.com>
2022-09-24 15:04:33 -0300

    resolução Exercicio22

--- src/exercicio02/Exercicio22.java
+++ src/exercicio02/Exercicio22.java
@@ -2,5 +2,19 @@
         par ou impar. Dica: utilize o operador módulo (resto da divisão)*/
 package exercicio02;
 
+import java.util.Scanner;
+
 public class Exercicio22 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Entre com um numero para que descobrir se é IMPAR ou PAR: ");
+        int numero = input.nextInt();
+
+        if (numero % 2 == 0){
+            System.out.println("Esse numero é PAR!");
+        } else {
+            System.out.println("Esse numero é IMPAR!");
+        }
+    }
 }
